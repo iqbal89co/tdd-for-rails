e11278dedf9bf2bbc1159f8ad145a5b8af5df786
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-18 02:31:34 +0700

    add spec 'new' for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'foods/index'
   get '/foods/show/:id', to: "foods#show"
+  get '/foods/new', to: "foods#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
