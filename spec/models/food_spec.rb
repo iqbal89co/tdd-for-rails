de20068d0e0934025f401d836b2f8a3120d07ba9
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-18 01:47:13 +0700

    refactor food_spec with factory

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -6,91 +6,33 @@ RSpec.describe Food, type: :model do
   end
   describe 'self#by_letter' do
     it "should return a sorted array of results that match" do
-      food1 = Food.create(
-        name: "Nasi Uduk",
-        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0,
-        category_id: 1
-      )
-
-      food2 = Food.create(
-        name: "Kerak Telor",
-        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
-        price: 8000.0,
-        category_id: 1
-      )
-
-      food3 = Food.create(
-        name: "Nasi Semur Jengkol",
-        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
-        price: 8000.0,
-        category_id: 1
-      )
+      food1 = FactoryBot.create(:food, name: 'Nasi Uduk')
+      food2 = FactoryBot.create(:food, name: 'Kerak Telor')
+      food3 = FactoryBot.create(:food, name: 'Nasi Semur Jengkol')
 
       expect(Food.by_letter("N")).to eq([food3, food1])
     end
   end
   it "is invalid with price which have non numeric values" do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: "hayaa",
-        category_id: 1
-    )
-
-    expect(food).to_not be_valid
+    expect(FactoryBot.build(:food, price: "haha")).to_not be_valid
   end
   it "is invalid with price less than 0.01" do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 0.001,
-        category_id: 1
-    )
-
-    expect(food).to_not be_valid
+    expect(FactoryBot.build(:food, price: 0.001)).to_not be_valid
   end
   it 'is valid with a name and a description' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0,
-      category_id: 1
-    )
-
-    expect(food).to be_valid
+    expect(FactoryBot.build(:food)).to be_valid
   end
 
   it 'is invalid without a name' do
-    food = Food.new(
-      name: nil,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0,
-        category_id: 1
-    )
-
+    food = FactoryBot.build(:food, name: nil)
     food.valid?
-
     expect(food.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate name" do
-    food1 = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0,
-        category_id: 1
-    )
-    
-    food2 = Food.new(
-      name: "Nasi Uduk",
-      description: "Just with a different description.",
-      price: 10000.0,
-      category_id: 1
-    )
-
+    food1 = FactoryBot.create(:food, name: 'Nasi Uduk')
+    food2 = FactoryBot.build(:food, name: 'Nasi Uduk')
     food2.valid?
-    
     expect(food2.errors[:name]).to include("has already been taken")
   end
 end
