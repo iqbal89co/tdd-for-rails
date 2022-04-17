56ba24c2dcec46596870ee225504bb87006821bf
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-18 02:20:15 +0700

    add new controller, routes, and views for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get '/foods/show/:id', to: "foods#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
